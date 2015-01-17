5f2d46294121f97847ed12ce86dae5bd6d39f6d1
JustinChee <JustinChee@192.168.1.85>
2015-01-16 20:20:35 -0800

    Finished tutorial 2c Signed-off-by:JustinChee <JustinChee@192.168.1.85> Signed-off-by:JustinChee <JustinChee@192.168.1.85>

---
+++ application/controllers/Gallery.php
@@ -0,0 +1,33 @@
+<?php
+
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Gallery extends MY_Controller {
+
+    public function index()
+    {
+        // Get all the images from out model
+        $pix = $this->images->all();
+        
+        // Build an array of formatted cells for them
+        foreach ($pix as $picture)
+            $cells[] = $this->parser->parse('_cell', (array) $picture, true);
+        
+        // Prime the table class
+        $this->load->library('table');
+        $parms = array(
+            'table_open' => '<table class="gallery">',
+            'cell_start' => '<td class="oneimage">',
+            'cell_alt_start' => '<td class="oneimage">'
+        );
+        $this->table->set_template($parms);
+        
+        // Finally! Generate the table
+        $rows = $this->table->make_columns($cells, 3);
+        $this->data['thetable'] = $this->table->generate($rows);
+        
+        $this->data['pagebody'] = 'gallery';
+        $this->render();
+    }
+    
+}
